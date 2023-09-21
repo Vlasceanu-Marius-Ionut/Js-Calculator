3d9827bdcae0894fcbaee41a451a26c1efc34252
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-21 17:21:00 +0300

    update

--- js.js
+++ js.js
@@ -1,3 +1,27 @@
 'use strict'
 
+let calcontainer = document.getElementById('call');
+let buttons = document.querySelector('button');
 
+let stringer = "";
+let arr = Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'Ac'){
+            string =""
+            input.value = string
+        }
+        else if(e.target.innerHTML == 'Del'){
+            string = string.substring(0, string.lenght-1);
+            input.value = string
+        }else {
+            string += e.tarrget.innerHTML;
+            input.value = string
+        }
+    })
+})
