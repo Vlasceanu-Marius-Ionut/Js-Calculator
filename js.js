a116328ca0c8098a8e0160754daca1678d96d320
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-09-21 13:46:29 +0300

    third commit

--- js.js
+++ js.js
@@ -3,6 +3,8 @@
 const calculatorpatrrat = document.getElementById('calculatorpatrrat');
 const ecran = document.getElementById('ecranul');
 const butonie = document.querySelector('button');
+
+
 let num1 = ''
 let num2 = ''
 let operator = ''
@@ -10,19 +12,28 @@ let operator = ''
 
 butonie.addEventListener('click'),() =>{
     const buttonText = buttonText.textContent;
-
-    if(isNaN(buttonText) || buttonText === '.'){
-        if(operator === '+'){
+    //WEE CRATE A BUTTON VARIABLE
+    if(isNaN(buttonText) || buttonText === '.'){//IF THE BUTTONTEXT ISNAN = NOT A NUMBER = TRUE || FALSE
+        if(operator === '+'){ //IF OPERATOR === + WILL CALCULATE NUM1 + WHAT WE TYPE(BUTTONTEXT)
             num1 += buttonText;
         }else {
             num2 += buttonText;
         }
-    }else if(buttonText === '+' || buttonText === '-' || buttonText === '*' || buttonText === '/'){
+    }else if(buttonText === '+' || buttonText === '-' || buttonText === '*' || buttonText === '/'){ //CALCULATOR BUTTONS
         operator = buttonText;
     }else if( buttonText === '='){
-        if(num1 !== '' && num2 !== '' && operator !== ''){
+        if(num1 !== '' && num2 !== '' && operator !== ''){ //RESULT, VA VERIFICCA DACA ARE SIR GOL
             const rezultat = calculator(operator, parseFloat(num1), parseFloat(num2));
-        }ecran.textContent = `Rezultat:${rezultat}`
+        }ecran.textContent = `Rezultat:${rezultat}`;
+
+        num1 = rezultat.toString();
+        num2 = '';
+        operator = '';
+    }
+    else if (button === 'C'){
+        num1 = '';  //CLEAN
+        num2 = '';
+        operator = '';
     }
 }
 
